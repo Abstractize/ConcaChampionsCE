0c93cc91caeef7d732c5d1a906e375bd6e9aac33
BerthaBrenes <bertacaro1996@gmail.com>
2019-03-09 19:16:35 -0600

    [add] fshow all the players

--- ejerciciosUI/Cancha.rkt
+++ ejerciciosUI/Cancha.rkt
@@ -1,6 +1,7 @@
 #lang racket
 ;Tutorial
 (require (lib "graphics.ss" "graphics"))
+(require htdp/draw)
 (open-graphics)
 
 ;ventana principal de la cancha
@@ -37,14 +38,39 @@
 ;Delimitadores
 ((clear-solid-rectangle CampoJugadores) (make-posn 1000 0) 4 500)
 ((clear-solid-rectangle CampoJugadores) (make-posn 1000 100) 200 4)
-
+((draw-pixmap CampoJugadores) "/Users/bertha/Documents/Dr.Racket/ConcaChampionsCE/ejerciciosUI/cancha.bmp" (make-posn 0 0))
+((draw-pixmap CampoJugadores) "/Users/bertha/Documents/Dr.Racket/ConcaChampionsCE/ejerciciosUI/bola.bmp" (make-posn 500 220))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;Creacion de jugadores
 ;posx => posicion en x
 ;posy => posicion en y
 ;lad => tecla que mueve el jugador, Preguntar al profe(aqui creo que va el algoritmo genetico)
-(define (jugadores posx posy lad )
+(define (jugadoresG1 posx posy lad )
+  (if (equal? lad 'u)
+      ;((draw-solid-rectangle CampoJugadores) (make-posn posx posy) 10 10 "black")
+      ((draw-pixmap CampoJugadores) "/Users/bertha/Documents/Dr.Racket/ConcaChampionsCE/ejerciciosUI/defensa.bmp" (make-posn posx posy))
+      (if (equal? lad 'd)
+          ;((draw-solid-rectangle CampoJugadores) (make-posn posx posy) 10 10 "black")
+          ((draw-pixmap CampoJugadores) "/Users/bertha/Documents/Dr.Racket/ConcaChampionsCE/ejerciciosUI/defensa.bmp" (make-posn posx posy))
+          (if (equal? lad 'l)
+              ;((draw-solid-rectangle CampoJugadores) (make-posn posx posy) 10 10 "black")
+              ((draw-pixmap CampoJugadores) "/Users/bertha/Documents/Dr.Racket/ConcaChampionsCE/ejerciciosUI/defensa.bmp" (make-posn posx posy))
+              (if (equal? lad 'r)
+                  ;((draw-solid-rectangle CampoJugadores) (make-posn posx posy) 10 10 "black")
+                  ((draw-pixmap CampoJugadores) "/Users/bertha/Documents/Dr.Racket/ConcaChampionsCE/ejerciciosUI/defensa.bmp" (make-posn posx posy))
+                  ;else
+                  (void)
+                  )
+              )
+      )
+ )
+  
+ (copy-viewport CampoJugadores ventanaPrincipal)
+  ;((clear-viewport CampoJugadores))
+  )
+
+(define (jugadoresG2 posx posy lad )
   (if (equal? lad 'u)
       ;((draw-solid-rectangle CampoJugadores) (make-posn posx posy) 10 10 "black")
       ((draw-pixmap CampoJugadores) "/Users/bertha/Documents/Dr.Racket/ConcaChampionsCE/ejerciciosUI/delantero.bmp" (make-posn posx posy))
@@ -62,7 +88,8 @@
                   )
               )
       )
-      )
+ )
+  
  (copy-viewport CampoJugadores ventanaPrincipal)
   ;((clear-viewport CampoJugadores))
   )
@@ -74,45 +101,55 @@
 ;posx => posicion en x
 ;posy => posicion en y
 ;press => la tecla que se esta presionando
-(define (teclado limR limI posx posy press)
+(define (teclado limR limI posx posy press generacion)
+
+        
   ;limite derecha
   (if (< posx limR)
       (begin
-        (jugadores limR posy 'r)
-        (teclado limR limI limR posy (key-value(get-key-press ventanaPrincipal))))
+        (jugadoresG1 limR posy 'r)
+        (jugadoresG1 limR (+ posy 200) 'r)
+        (teclado limR limI limR posy (key-value(get-key-press ventanaPrincipal)) (- generacion 1)))
       ;limite de la izquiera TODO un parametro
   (if (> posx (- limI 10))
       (begin
-        (jugadores (- limI 10) posy 'r)
-        (teclado limR limI (- limI 10) posy (key-value(get-key-press ventanaPrincipal))))
+        (jugadoresG1 (- limI 10) posy 'r)
+        (jugadoresG1 (- limI 10) (+ posy 200) 'r)
+        (teclado limR limI (- limI 10) posy (key-value(get-key-press ventanaPrincipal)) (- generacion 1)))
       ;limite arriba
-  (if (< posy 0)
+  (if (< posy 0)((draw-pixmap CampoJugadores) "/Users/bertha/Documents/Dr.Racket/ConcaChampionsCE/ejerciciosUI/cancha.bmp" (make-posn 0 0))
       (begin
-        (jugadores posx 0 'u)
-        (teclado limR limI posx 0 (key-value(get-key-press ventanaPrincipal))))
+        (jugadoresG1 posx 0 'u)
+        (jugadoresG1 (+ posx 200) 0 'u)
+        (teclado limR limI posx 0 (key-value(get-key-press ventanaPrincipal)) (- generacion 1)))
       ;limite de abajo
   (if (> posy 490)
       (begin
-        (jugadores posx  490 'd)
-        (teclado limR limI posx 490 (key-value(get-key-press ventanaPrincipal))))    
+        (jugadoresG1 posx  490 'd)
+        (jugadoresG1 (+ posx 200) 490 'd)
+        (teclado limR limI posx 490 (key-value(get-key-press ventanaPrincipal)) (- generacion 1)))    
   (if (equal? press 'up)
       (begin
-        (jugadores posx posy 'u)
-        (teclado limR limI posx (- posy 10) (key-value (get-key-press ventanaPrincipal))))
+        (jugadoresG1 posx posy 'u)
+        (jugadoresG1 (+ posx 200) (+ posy 200) 'u)
+        (teclado limR limI posx (- posy 10) (key-value (get-key-press ventanaPrincipal)) (- generacion 1)))
       (if (equal? press 'down)
           (begin
-            (jugadores posx posy 'd)
-            (teclado limR limI posx (+ posy 10) (key-value (get-key-press ventanaPrincipal))))
+            (jugadoresG1 posx posy 'd)
+            (jugadoresG1 (+ posx 200) (+ posy 200) 'd)
+            (teclado limR limI posx (+ posy 10) (key-value (get-key-press ventanaPrincipal)) (- generacion 1)))
       (if (equal? press 'left)
           (begin
-            (jugadores posx posy 'l)
-            (teclado limR limI (- posx 10) posy (key-value (get-key-press ventanaPrincipal))))
+            (jugadoresG1 posx posy 'l)
+            (jugadoresG1 (+ posx 200) (+ posy 200) 'l)
+            (teclado limR limI (- posx 10) posy (key-value (get-key-press ventanaPrincipal)) (- generacion 1)))
       (if (equal? press 'right)
           (begin
-            (jugadores posx posy 'r)
-            (teclado limR limI (+ posx 10) posy (key-value (get-key-press ventanaPrincipal))))
+            (jugadoresG1 posx posy 'r)
+            (jugadoresG1 (+ posx 200) (+ posy 200) 'r)
+            (teclado limR limI (+ posx 10) posy (key-value (get-key-press ventanaPrincipal)) (- generacion 1)))
           ;else
-          (teclado limR limI posx posy (key-value (get-key-press ventanaPrincipal)))
+          (teclado limR limI posx posy (key-value (get-key-press ventanaPrincipal)) (- generacion 1))
           )
     )
       )
@@ -120,29 +157,109 @@
   )
   ))
   ))
+
+
+
+(define (transformar AliG1 AliG2)
+  (cond ((and (null? AliG1) (null? AliG2))
+         void)
+        (else
+      (begin
+        (jugadoresG1 (car (car AliG1)) (car (cdr (car AliG1))) 'r)
+        (jugadoresG2 (car (car AliG2)) (car (cdr (car AliG2))) 'r)
+        (transformar (cdr AliG1) (cdr AliG2)))
+      )
+     )
+  )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;defensas ;;;;;;;;;;
+(define (defensaG1 num)
+  (cond ((zero? num)
+         '())
+        (else
+         (cons (list 290 (/ 500 (+ 1 num))) (defensaG1 (- num 1)) ) 
+            )))
+(define (defensaG2 num)
+  (cond ((zero? num)
+         '())
+        (else
+         (cons (list 725 (/ 500 (+ 1 num))) (defensaG2 (- num 1)) ) 
+            )))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;delanteros;;;;
+(define (delanteroG1 num)
+  (cond ((zero? num)
+         '())
+        (else
+         (cons (list 680 (+ 100 (/ 500 (+ 1 num)))) (delanteroG1 (- num 1)) ) 
+            )))
+(define (delanteroG2 num)
+  (cond ((zero? num)
+         '())
+        (else
+         (cons (list 250 (+ 100 (/ 500 (+ 1 num)))) (delanteroG2 (- num 1)) ) 
+            )))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;medios
+(define (mediosG1 num)
+  (cond ((zero? num)
+         '())
+        (else
+         (cons (list 495 (/ 500 (+ 1 num))) (mediosG1 (- num 1)) ) 
+            )))
+(define (mediosG2 num)
+  (cond ((zero? num)
+         '())
+        (else
+         (cons (list 450 (/ 500 (+ 1 num))) (mediosG2 (- num 1)) ) 
+            )))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;Crear Grupo 1;;;;;;;
+(define (G1 lista)
+  (cond ((null? lista)
+         porteroG1)
+        ((= (largo lista) 3)
+         (append  (defensaG1 (car lista)) (G1 (cdr lista))))
+        ((= (largo lista) 2)
+         (append  (mediosG1 (car lista)) (G1 (cdr lista))))
+        ((= (largo lista) 1)
+         (append  (delanteroG1 (car lista)) (G1 (cdr lista))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;Crear G2;;;;;;;;;
+(define (G2 lista)
+  (cond ((null? lista)
+         porteroG2)
+        ((= (largo lista) 3)
+         (append  (defensaG2 (car lista)) (G2 (cdr lista))))
+        ((= (largo lista) 2)
+         (append  (mediosG2 (car lista)) (G2 (cdr lista))))
+        ((= (largo lista) 1)
+         (append  (delanteroG2 (car lista)) (G2 (cdr lista))))))
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;largo lista;;;;;;;;;;
+(define (largo lista)
+  (cond((empty? lista)
+        0)
+       (else
+        (+ 1 (largo (cdr lista))))))
+;;;;;;;;;;;;;;;;;;
+;;;;;transformar
+(define (CCCE2019 AlG1 AlG2 G)
+  (cond ((zero? G)
+        (void))
+        (else
+         ((transformar (G1 AlG1) (G2 AlG2)) (CCCE2019 AlG1 AlG2 (- 1 G))))))
+  
+
 (define porteroG1
-  '(0 50 10 250)
+  '((25 250))
   )
-(define defensaG1 
-  '(50 200 60 250)
-  )
-(define medioG1
-  '(200 350 250 250)
-  )
-(define delanteroG1
-  '(350 500 400 250))
-
-(define delanteroG2
-  '(500 650 550 250))
-
-(define medioG2
-  '(650 800 700 250))
-
-(define defensaG2
-  '(800 950 900 250))
-
 (define porteroG2
-  '(950 1000 1040 250))
+  '((970 250))
+  )
 
-(teclado 350 500 400 250 'up)
+;(teclado 800 950 900 250 'up 10)
   ;(teclado 0 50 10 250 'up)
+
+;(CCCE20192 '((50 82) (150 164) (240 246) (200 328) (350 410) (400 125) (380 250) (500 375) (750 260) (750 320)) '((850 100) (850 200) (850 300) (850 400) (600 100) (600 200) (600 300) (600 400) (350 160) (350 320)))
+(CCCE2019 '(4 4 2) '(4 2 4) 1)
